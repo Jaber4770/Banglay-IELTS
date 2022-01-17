83cadcd0c57f254ee85f7bb632669f6794b27ea2
jaber4770 <jaber.ahmed4770@gmail.com>
2022-01-18 00:48:11 +0600

    about component done

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import "./About.css"
+import img from '../../images/Banglay-IELTS-Profile.jpg'
+import qsnImg from '../../images/question.jpg'
 
 const About = () => {
     return (
-        <div className='bg-color'>
+        <div className=''>
             <Container>
                 <div className='p-3'>
                     <div>
@@ -17,35 +19,42 @@ const About = () => {
                         <p className='ielts-result'>আপনার স্কোরের গ্যারান্টি দিতে পারবো না, তবে IELTS এর ভয়ডর দূর করে দিবো।
                             No worries, <b>InSha'Allah</b> valo kicu paben 😊</p>
                     </div>
-                    <div>
-                        <h4>Course Details</h4>
-                        <p>☆ 2 months duration.</p>
-                    </div>
-                    <div>
-                        <h4>Classes</h4>
-                        <p>☆ Total 30+ classes. <br />
-                            ☆ MINIMUM Number of Classes: <br />
-                            ☆ Reading 10 classes <br />
-                            ☆ Writing 10 classes <br />
-                            ☆ Listening 8 classes <br />
-                            ☆ Speaking 5 classes <br />
-                            ☆ Grammar and Structure 2 classes <br />
-                            ☆ Friday Night Free Live Class <br />
-                            ☆ Unlimited Solve Classes <br />
-                            ☆ Per Week 5 classes = 3 main classes + 1 Solve Class + 1 Free live class (every Friday night with a lot of unknown bhai brothers 😊)</p>
-                    </div>
-                    <div>
-                        <h4>Platform:</h4>
-                        <h5>☆ Online (Zoom)</h5>
-                    </div>
-                    <div>
-                        <h4>About you</h4>
-                        <p>☆ We will ask a few questions before adding you in the DREAM BATCH ❤️</p>
+                    <div className='d-flex'>
+                        <div className='w-50'>
+                            <div>
+                                <h4>Course Details</h4>
+                                <p>☆ 2 months duration.</p>
+                            </div>
+                            <div>
+                                <h4>Classes</h4>
+                                <p>☆ Total 30+ classes. <br />
+                                    ☆ MINIMUM Number of Classes: <br />
+                                    ☆ Reading 10 classes <br />
+                                    ☆ Writing 10 classes <br />
+                                    ☆ Listening 8 classes <br />
+                                    ☆ Speaking 5 classes <br />
+                                    ☆ Grammar and Structure 2 classes <br />
+                                    ☆ Friday Night Free Live Class <br />
+                                    ☆ Unlimited Solve Classes <br />
+                                    ☆ Per Week 5 classes = 3 main classes + 1 Solve Class + 1 Free live class (every Friday night with a lot of unknown bhai brothers 😊)</p>
+                            </div>
+                            <div>
+                                <h4>Platform:</h4>
+                                <h5>☆ Online (Zoom)</h5>
+                            </div>
+                            <div>
+                                <h4>About you</h4>
+                                <p>☆ We will ask a few questions before adding you in the DREAM BATCH ❤️</p>
+                            </div>
+                        </div>
+                        <div className='w-50'>
+                            <img className='img-fluid' src={img} alt="" />
+                        </div>
                     </div>
                     <div>
                         <h4>Please Read Carefully:</h4>
                         <p>☆ This is a two-month plan. COURSE FEE TOTAL 8,000 TAKA. You have to pay at the BEGINNING.
-                            NO instalment and NO Consideration. So, Please DON’T Ask!
+                            <span className='ielts-result'> NO instalment and NO Consideration.</span> So, Please DON’T Ask!
                         </p>
                     </div>
                     <div>
@@ -57,7 +66,14 @@ const About = () => {
                         <h5>5/ Cambridge boi ase? Thakle Cambridge boi theke ekhn kmn correct hoy, like listening and reading e 40 e koyta correct hoy?
                             r Cambridge boi na thakle, age boi kinen then exam (a full listening and a full reading) diye amk janan.</h5>
                         <h5>6/ Anything you think We NEED to know about you such as previous exam scores, any fun fact about you, or your workplace/experiences etc.</h5>
-                        <h5>☆ Again, If you are HIGHLY interested to join us, please answer the above questions. I will reply further details (day, timing and payment procedure) in my CONVENIENT time. So, please have patience and wait.</h5>
+                    </div>
+                    <div className='d-flex'>
+                        <div className='w-50'>
+                            <img className='img-fluid' src={qsnImg} alt="question" />
+                        </div>
+                        <div className='w-50 mt-5'>
+                            <h5>☆ Again, If you are HIGHLY interested to join us, please answer the above questions. I will reply further details (day, timing and payment procedure) in my CONVENIENT time. So, please have patience and wait.</h5>
+                        </div>
                     </div>
                 </div>
             </Container>
