9b5d35a8e03dd8eaf3881eaafe786258f5233137
jaber4770 <jaber.ahmed4770@gmail.com>
2022-01-18 20:24:38 +0600

    icon imported done

--- src/component/ServicesCart/ServicesCart.js
+++ src/component/ServicesCart/ServicesCart.js
@@ -1,15 +1,34 @@
 import { Card, Col, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLeanpub } from '@fortawesome/free-brands-svg-icons';
+import { faBookReader, faBriefcase, faChartBar, faDoorOpen, faEnvelopeOpenText, faGraduationCap, faHeadphones, faLightbulb, faLink, faMicrophoneAlt, faPencilAlt, faWifi } from '@fortawesome/free-solid-svg-icons'
 
 const ServicesCart = (props) => {
-    const { GT, academic, img, listening, name, AcademicGT, reading, solveClass, speaking, writting } = props.service;
+    const { GT, academic, img, listening, name, AcademicGT, reading, speaking, writting } = props.service;
     return (
         <div>
+
+            <FontAwesomeIcon icon={faWifi} />
+            <FontAwesomeIcon icon={faHeadphones} />
+            <FontAwesomeIcon icon={faBookReader} />
+            <FontAwesomeIcon icon={faPencilAlt} />
+            <FontAwesomeIcon icon={faBriefcase} />
+            <FontAwesomeIcon icon={faMicrophoneAlt} />
+            <FontAwesomeIcon icon={faGraduationCap} />
+            <FontAwesomeIcon icon={faLeanpub} />
+            <FontAwesomeIcon icon={faEnvelopeOpenText} />
+            <FontAwesomeIcon icon={faChartBar} />
+            <FontAwesomeIcon icon={faDoorOpen} />
+            <FontAwesomeIcon icon={faLightbulb} />
+            <FontAwesomeIcon icon={faLink} />
+
+
             <Container className='mt-4 mb-3'>
                 <Col>
                     <Card style={{ width: '15rem', height: '15rem' }}>
                         <Card.Img variant="top" src={img} />
                         <Card.Body className='text-center'>
-                            <Card.Title><h2>{name}</h2></Card.Title>
+                            <Card.Title><h1>{name}</h1></Card.Title>
                             <Card.Text>
                                 <h4>{listening}</h4>
                                 <h4>{reading}</h4>
