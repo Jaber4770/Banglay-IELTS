a3a2072dc38b2111600f02551271591a5f14dc80
jaber4770 <jaber.ahmed4770@gmail.com>
2022-01-18 23:38:27 +0600

    services modified

--- src/component/Service/Service.js
+++ src/component/Service/Service.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import ServicesCart from '../ServicesCart/ServicesCart';
+import './Service.css'
 
 const Service = () => {
     // There i am loading data from ServicesData, public folder and converting data to js from json and sendint data to servicesCart by using map.
@@ -15,7 +16,7 @@ const Service = () => {
     return (
         <Container className='mt-3 mb-5'>
             <div>
-                <h3 className='text-center bg-color p-3 text-light mb-4 why-chs-bg'>Our Services</h3>
+                <h3 className='text-center bg-color p-3 mb-4 Our-services'>Our Services</h3>
             </div>
             <div>
                 <Row xs={1} md={2} lg={4} >
